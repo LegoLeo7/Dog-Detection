879597caee77da6e2974cfd73f6d217749e41459
LegoLeo7 <99499098+LegoLeo7@users.noreply.github.com>
2022-12-12 18:32:18 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+function preload(){
+    img = loadImage('dog_cat.jpg');
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+img = "";
+status = "";
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+   fill("#0000FF");
+   text("Dog", 45, 75);
+   noFill();
+   stroke("#0000FF");
+   rect(30, 60, 450, 350 );
+
+   fill("#0000FF");
+   text("Cat", 320, 120);
+   noFill();
+   stroke("#0000FF")
+   rect(300, 90, 270, 320)
+   
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!")
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results) {
+    if(error) {
+        console.log(error);
+    }
+    console.log(results);
+}
